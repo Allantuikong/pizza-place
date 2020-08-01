657ca93702766c2551364d0936eb4c8d933bf8a7
[Allantuikong] <allantuix@gmail.com>
2020-08-01 14:49:14 +0300

    Add js code with prices for differenet pizza sizes

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//pizza types
+let pizzaType1 = {
+    name: "Meat Deluxe",
+    price: [],
+}
+let pizzaType2 = {
+    name: "Peri peri chicken",
+    price: [],
+}
+let pizzaType3 = {
+    name: "Chicken BBQ",
+    price: [],
+}
+let pizzaType4 = {
+    name: "Indian Veg Feast",
+    price: []
+}
+//pizza sizes & prices
+let pizzaSize1 = {
+    name: "Kapyenga(small)",
+    price: 500
+}
+let pizzaSize2 = {
+    name: "Medium",
+    price: 700
+}
+let pizzaSize3 = {
+    name: "Large",
+    price: 1000
+}
+let pizzaSize4 = {
+    name: "Uzito(x-large",
+    price: 1200
+}
