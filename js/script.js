a1baa72d4fbb6f909942d3d7fef245262ac2ca94
[Allantuikong] <allantuix@gmail.com>
2020-08-04 00:40:42 +0300

    Add js functionality

--- js/script.js
+++ js/script.js
@@ -8,6 +8,71 @@ class Pizza {
     }
 }
     
+$(document).ready(function() {
+    $("#orderPizza").submit(function(event){
+        event.preventDefault()
+        
+          function type() {
+            let pizzaType = document.getElementById("pizza_type").value;
+            return parseInt(pizzaType);
+          }
+          function size() {
+            let pizzaSize = document.getElementById("pizza_size").value;
+            return parseInt(pizzaSize);
+          }
+          function crust() {
+            let pizzaCrust = document.getElementById("pizza_crust").value;
+            return parseInt(pizzaCrust);
+          }
+          function toppings() {
+            let pizzaToppings = document.getElementById("pizza_toppings").value;
+            return parseInt(pizzaToppings);
+          }
+          function number() {
+            let pizzaNumber = document.getElementById("quantity").value;
+             return parseInt(pizzaNumber);
+          }
+
+        let userInput = new Pizza (type(), size(), crust(), toppings(), number())
+    
+        let totalCost = (userInput.newType + userInput.newSize + userInput.newCrust + userInput.newToppings) * userInput.newQuantity
+
+    
+        $("#order-summary").html("Your charges for the pizza is Ksh. " + " " +  totalCost + "Hope you enjoy this meal!");
+
+        $("input#pizza_size").val("")
+        $("input#pizza_crust").val("")
+        $("input#pizza_type").val("")
+        $("input#pizza_toppings").val("")
+        $("input#quantity").val ("")
+        $("input#order-summary").val ("")
+    
+        
+    })
+
+    $("#deliveryPointForm").submit(function(event) {
+        event.preventDefault();
+      
+        deliveryPoint = $("#deliveryPoint").val();
+        if (deliveryPoint == 1) {
+            $("#remoteLocation").show();
+      
+        };
+      
+    });
+    
+    $("#submitLocaton").submit(function(event) {
+        event.preventDefault();
+        var remoteClientName = $("#userName").val();
+        var remoteClientAddress = $("#userAddress").val();
+        var remoteClientPhoneNumber = $("#userTelNum").val();
+      
+        $("#remoteThankYou").html("Thankyou " + remoteClientName + " for odering with us! Your order will be delivered to your location, " + remoteClientAddress + ".");
+      
+      });
+
+})
+ 
     // calculateCost() {
 
 
@@ -34,68 +99,7 @@ class Pizza {
     //     });
 
     //     return this.cost;
-    // }
-
-$(document).ready(function() {
-    $("#orderPizza").submit(function(event){
-        event.preventDefault()
-        function type() {
-            let pizzaType = document.getElementById("pizza_type").value;
-            return parseInt(pizzaType);
-          }
-          function size() {
-            let pizzaSize = document.getElementById("pizza_size").value;
-            return parseInt(pizzaSize);
-          }
-          function crust() {
-            let pizzaCrust = document.getElementById("pizza_crust").value;
-            return parseInt(pizzaCrust);
-          }
-          function toppings() {
-            let pizzaToppings = document.getElementById("pizza_toppings").value;
-            return parseInt(pizzaToppings);
-          }
-          function number() {
-            let pizzaNumber = document.getElementById("quantity").value;
-             return parseInt(pizzaNumber);
-          }
-
-
-    
-        let userInput = new Pizza (type(), size(), crust(), toppings(), number());
-    
-        var totalCost = (userInput.newType + userInput.newSize + userInput.newCrust + userInput.newToppings) *userInput.newQuantity;
-    
-        alert("Your total cost is Ksh." + " " + totalCost);
-
-
-
-    })
-
-    $("#deliveryPointForm").submit(function(event) {
-        event.preventDefault();
-      
-        deliveryPoint = $("#deliveryPoint").val();
-        if (deliveryPoint == 1) {
-            $("#remoteLocation").show();
-      
-        };
-      
-    });
-    
-    $("#submitLocaton").submit(function(event) {
-        event.preventDefault();
-        var remoteClientName = $("#userName").val();
-        var remoteClientAddress = $("#userAddress").val();
-        var remoteClientPhoneNumber = $("#userTelNum").val();
-      
-        $("#remoteThankYou").html("Thankyou " + remoteClientName + " for odering with us! Your order will be delivered to your location, " + remoteClientAddress + ".");
-      
-      });
-
-})
- 
-     
+    // }   
     
 
      
