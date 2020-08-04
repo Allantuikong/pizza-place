2e269e19f533b3b41a76bca5400a9c3b1561f66e
[Allantuikong] <allantuix@gmail.com>
2020-08-04 09:20:31 +0300

    Add final changes to work

--- js/script.js
+++ js/script.js
@@ -13,40 +13,42 @@ $(document).ready(function() {
     $("#order_Pizza").submit(function(event){
         event.preventDefault()
         
+        
             function type() {
-                let pizzaType = document.getElementById("pizza_type").value;
+                let pizzaType = document.getElementById("pizza_type").value
                 return parseInt(pizzaType);
             }
 
             function size() {
-                let pizzaSize = document.getElementById("pizza_size").value;
+                let pizzaSize = document.getElementById("pizza_size").value
                 return parseInt(pizzaSize);
             }
 
             function crust() {
-                let pizzaCrust = document.getElementById("pizza_crust").value;
+                let pizzaCrust = document.getElementById("pizza_crust").value
                 return parseInt(pizzaCrust);
             }
 
             function toppings() {
-                let pizzaToppings = document.getElementById("pizza_toppings").value;
+                let pizzaToppings = document.getElementById("pizza_toppings").value
                 return parseInt(pizzaToppings);
             }
             
             function number() {
-                let pizzaNumber = document.getElementById("quantity").value;
+                let pizzaNumber = document.getElementById("quantity").value
                 return parseInt(pizzaNumber);
             }
 
 
     
         let userInput = new Pizza (type(), size(), crust(), toppings(), number() );
+
+        let totalCost = (userInput.newType + userInput.newSize + userInput.newCrust + userInput.newToppings) * userInput.newQuantity
     
-        let totalCost = (userInput.newType + userInput.newSize + userInput.newCrust + userInput.newToppings) * userInput.newQuantity;
-    
-    
-       $("#order-summary").html("Thankyou  for ordering with us! " + "your total cost is Ksh." + "" + totalCost  );
-        
+     
+       $("#order-summary").html("Thank you  for ordering with us! " + "your total cost is Ksh." + "" + totalCost  );
+
+       
     })
 
     $("#deliveryPointForm").submit(function(event) {
@@ -67,7 +69,7 @@ $(document).ready(function() {
       
         $("#remoteThankYou").html("Thankyou " + remoteClientName + " for odering with us! Your order will be delivered to your location, " + remoteClientAddress + ".");
       
-      });
+    });
 
 })
  
